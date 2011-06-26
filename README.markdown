948f46e8285bc4e72751bd8ae592e961c9b94d5f
Jonathan Mifsud <info@jonmifsud.com>
2011-06-26 10:00:08 +0200

    Upgraded to version 1.3 Added direction funcionality so ticker can now move both right to left and left to right. More information in the readme & example files

--- README.markdown
+++ README.markdown
@@ -1,31 +1,53 @@
-#Web Ticker v1.1
+#Web Ticker v1.3
 
 ##How to use
 
 $('#webTicker').webTicker(settings);
+
 //where webTicker is the id of the container you would like the ticker to run upon above function starts ticker rotating
 
 $('#webTicker').webTicker('stop');
+
 //stop the ticker from moving
 
 $('#webTicker').webTicker('cont');
+
 //continue ticker movement from where it was before (same settings as previously will be used)
 
 
 ## Settings - Optional
 
-The settings at this point in time only include the speed of the ticker which is set by default to 0.05
-Speed changes are relatively undested and setting it too fast might cause unexpected results
+The settings at this point in time only include the speed of the ticker which is set by default to 0.05 as well as the direction of the ticker.
+
+Speed changes are relatively undested and setting it too fast might cause unexpected results.
+
+Also note that speed values in the negative domain will not change the direction of flow but rather make the ticker not work. A separate value is used for direction settings and this can be either 1 for right to left movement or -1 for left to right movement.
+
+Please not that in any case the first item will be the first that goes out of screen.
 
 travelocity: 0.05
+direction: 1
 
 ##CSS Requirements:
 
 .tickercontainer
+
 //width of the original container in which the ticker appears
 
+float: left if direction is 1 otherwise float right
+
+
+.tickercontainer li
+
+//width of the original container in which the ticker appears
+
+float: left if direction is 1 otherwise float right
+
 .tickercontainer .mask
+
 //can be used to achieve padding within the container
+
+NB: if running multiple tickers it is suggested that you use the id rather then the class name should they have different directions.
 
 ### Optional
 
--- jquery.webticker.js
+++ jquery.webticker.js
@@ -1,9 +1,9 @@
 /*!
- * webTicker 1.2
+ * webTicker 1.3
  * Examples and documentation at: 
  * http://jonmifsud.com
  * 2011 Jonathan Mifsud
- * Version: 1.2 (29-JUNE-2011)
+ * Version: 1.2 (26-JUNE-2011)
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
@@ -14,15 +14,15 @@
 (function( $ ){
 
   var globalSettings = new Array();
-  var moving;
 
   var methods = {
     init : function( settings ) { // THIS 
 		settings = jQuery.extend({
-			travelocity: 0.05
+			travelocity: 0.05,
+			direction: 1,
+			moving: true
 		}, settings);
 		globalSettings[jQuery(this).attr('id')] = settings;
-		moving = true;
 		return this.each(function(){
 				var $strip = jQuery(this);
 				$strip.addClass("newsticker")
@@ -35,18 +35,26 @@
 				});
 				$strip.width(stripWidth+200);//20 used for ie9 fix					
 				function scrollnews(spazio, tempo){
-					$strip.animate({left: '-='+ spazio}, tempo, "linear", function(){
-						$strip.children().last().after($strip.children().first());
-						var first = $strip.children().first();
-						var width = first.outerWidth(true);
-						var defTiming = width/settings.travelocity;
-						var tmpleft = $strip.css("left");
-						var outWidth = $strip.children().last().outerWidth(true);
-						var left = parseInt(tmpleft.replace('px','')) + outWidth;
+					if (settings.direction == 1)
+						$strip.animate({left: '-='+ spazio}, tempo, "linear", function(){
+							$strip.children().last().after($strip.children().first());
+							var first = $strip.children().first();
+							var width = first.outerWidth(true);
+							var defTiming = width/settings.travelocity;
 						//$strip.css("left", left);
-						$strip.css("left", '0');
-						scrollnews(width, defTiming);
-					});
+							$strip.css("left", '0');
+							scrollnews(width, defTiming);
+						});
+					else
+						$strip.animate({right: '-='+ spazio}, tempo, "linear", function(){
+							$strip.children().last().after($strip.children().first());
+							var first = $strip.children().first();
+							var width = first.outerWidth(true);
+							var defTiming = width/settings.travelocity;
+							//$strip.css("left", left);
+							$strip.css("right", '0');
+							scrollnews(width, defTiming);
+						});
 				}
 				
 				var first = $strip.children().first();
@@ -57,51 +65,64 @@
 					jQuery(this).stop();
 				},
 				function(){
-					if (moving){
+					if (globalSettings[jQuery(this).attr('id')].moving){
 						var offset = jQuery(this).offset();
 						var first = $strip.children().first();
 						var width = first.outerWidth(true);
-						var residualSpace = parseInt(jQuery(this).css('left').replace('px',''))+ width;
+						var residualSpace;
+						if (settings.direction == 1) residualSpace = parseInt(jQuery(this).css('left').replace('px',''))+ width;
+						else residualSpace = parseInt(jQuery(this).css('right').replace('px',''))+ width;
 						var residualTime = residualSpace/settings.travelocity;
-						scrollnews(residualSpace, residualTime);
+						scrollnews(residualSpace, residualTime);						
 					}
 				});			
 		});
 	},
     stop : function( ) { 
-		if (moving){
-			moving = false;
+		if (globalSettings[jQuery(this).attr('id')].moving){
+			globalSettings[jQuery(this).attr('id')].moving = false;
 			return this.each(function(){
 				jQuery(this).stop();
 			});
 		}
 	},
     cont : function( ) { // GOOD 	
-		if (!(moving)){
-			moving = true;
+		if (!(globalSettings[jQuery(this).attr('id')].moving)){
+			globalSettings[jQuery(this).attr('id')].moving = true;
 			var settings = globalSettings[jQuery(this).attr('id')];
 			return this.each(function(){
 				var $strip = jQuery(this);
 					function scrollnews(spazio, tempo){
-						$strip.animate({left: '-='+ spazio}, tempo, "linear", function(){
-							$strip.children().last().after($strip.children().first());
-							var first = $strip.children().first();
-							var width = first.outerWidth(true);
-							var defTiming = width/settings.travelocity;
-							var tmpleft = $strip.css("left");
-							var outWidth = $strip.children().last().outerWidth(true);
-							var left = parseInt(tmpleft.replace('px','')) + outWidth;
-							//$strip.css("left", left);
-							$strip.css("left", '0');
-							scrollnews(width, defTiming);
-						});
+							if (settings.direction == 1)
+								$strip.animate({left: '-='+ spazio}, tempo, "linear", function(){
+									$strip.children().last().after($strip.children().first());
+									var first = $strip.children().first();
+									var width = first.outerWidth(true);
+									var defTiming = width/settings.travelocity;
+								//$strip.css("left", left);
+									$strip.css("left", '0');
+									scrollnews(width, defTiming);
+								});
+							else
+								$strip.animate({right: '-='+ spazio}, tempo, "linear", function(){
+									$strip.children().last().after($strip.children().first());
+									var first = $strip.children().first();
+									var width = first.outerWidth(true);
+									var defTiming = width/settings.travelocity;
+									//$strip.css("left", left);
+									$strip.css("right", '0');
+									scrollnews(width, defTiming);
+								});
+						
 					}
 						var offset = jQuery(this).offset();
 						var first = $strip.children().first();
 						var width = first.outerWidth(true);
-						var residualSpace = parseInt(jQuery(this).css('left').replace('px','')) + width;
+						var residualSpace;
+						if (settings.direction == 1) residualSpace = parseInt(jQuery(this).css('left').replace('px',''))+ width;
+						else residualSpace = parseInt(jQuery(this).css('right').replace('px',''))+ width;
 						var residualTime = residualSpace/settings.travelocity;
-						scrollnews(residualSpace, residualTime);	
+						scrollnews(residualSpace, residualTime);
 							
 			});	
 		}
